bef3e9329dc422479647b7e9cf97116515bb9f84
Muneeb A. <133331694+muneeb787@users.noreply.github.com>
2024-12-02 11:00:09 -0800

    Update index.js

--- paraathi-gaali/stripe-backend/index.js
+++ paraathi-gaali/stripe-backend/index.js
@@ -47,7 +47,7 @@ module.exports = async (req, res) => {
       }
     }
     // Handle the Stripe webhook for payment success
-    else if (req.method === 'POST' && req.url === '/webhook') {  // Ensure route for webhook
+    else if (req.method === 'POST' && req.url === '/api/webhook') {  // Ensure route for webhook
       const sig = req.headers['stripe-signature'];
       const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;  // Use the webhook secret from the .env file
       const event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
